7f10a8915b907aa843402db8102d4c221417ee3b
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-06 11:17:25 +0700

    Write delNode Bool

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -59,6 +59,40 @@ class LinkedList
             return START == NULL;
         }
 
+        bool Search(int nim, Node **previous, Node **current)
+        {
+            *previous = START;
+            *current = START;
+            while (*current != NULL && (nim != (*current)->noMhs))
+            {
+                *previous = *current;
+                *current = (*current)->next;
+            }
+            return *current != NULL;
+            
+        }
+
+        bool delNode(int nim)
+        {
+            Node *current, *previous;
+            if (!Search(nim, &previous, &current))
+            {
+                
+                return false;
+            }
+            if (current == START)
+            {
+                START = START->next;
+            }
+            else
+            {
+                previous->next = current->next;
+            }
+            delete current;
+            return true;
+        }
+
+
     
 };
 

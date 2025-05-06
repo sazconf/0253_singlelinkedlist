d756c6e1c18d161eb7432d54906ca33e307f0f10
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-06 10:57:23 +0700

    complete add note procedure

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -13,5 +13,35 @@ class LinkedList
 {
         Node *START;
 
+        public:
+        LinkedList()
+        {
+            START = NULL;
+        }
+
+        void addNote()
+        {
+            int nim;
+            cout << "Masukkan Nomor Mahasiswa: ";
+            cin >> nim;
+
+            Node *nodeBaru = new Node;
+            nodeBaru->noMhs = nim;
+
+            if (START == NULL || nim <=  START->noMhs)
+            {
+                if((START != NULL) && (nim == START->noMhs))
+                {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n" << endl;
+                    return;
+                }
+                nodeBaru->next = START;
+                START = nodeBaru;
+                return;
+               
+            }
+        }
+
     
 };
+
